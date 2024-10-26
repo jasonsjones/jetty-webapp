6b7abdfd15c0b456320252aac56769108d26ecd7
Jason Jones <jsjones96@gmail.com>
2024-10-26 15:57:16 -0700

    feat: add logger

--- src/main/java/com/jasonsjones/CoreApp.java
+++ src/main/java/com/jasonsjones/CoreApp.java
@@ -1,7 +1,12 @@
 package com.jasonsjones;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CoreApp {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CoreApp.class);
+
     public static void main(String[] args) {
-        System.out.println("Core app started...");
+        LOGGER.info("Core app started...");
     }
 }
