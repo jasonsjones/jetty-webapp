27b6fb8b5e78f98cd478188787195ae599ecfdf9
Jason Jones <jsjones96@gmail.com>
2024-11-02 17:28:14 -0700

    feat: add main handler

--- src/main/java/com/jasonsjones/CoreServer.java
+++ src/main/java/com/jasonsjones/CoreServer.java
@@ -1,10 +1,27 @@
 package com.jasonsjones;
 
 import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+class CoreHandler extends Handler.Abstract {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CoreHandler.class);
+    @Override
+    public boolean handle(Request request, Response response, Callback callback) throws Exception {
+        LOGGER.info("Handling request...");
+
+        String user = "Guest";
+        String method = request.getMethod();
+        String path = request.getHttpURI().getPath();
+        LOGGER.info("{} => {} {}", user, method, path);
+
+        callback.succeeded();
+        return true;
+    }
+}
 
 public class CoreServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(CoreApp.class);
@@ -19,14 +36,8 @@ public class CoreServer {
 
         server.addConnector(connector);
 
-        server.setHandler(new Handler.Abstract() {
-            @Override
-            public boolean handle(Request request, Response response, Callback callback) throws Exception {
-                LOGGER.info("Handling request...");
-                callback.succeeded();
-                return true;
-            }
-        });
+        ContextHandler contextHandler = new ContextHandler(new CoreHandler(), "/");
+        server.setHandler(contextHandler);
     }
 
     public void start() throws Exception {
