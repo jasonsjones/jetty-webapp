0110883ef6424f5b85b18801d2c78cc25047c523
Jason Jones <jsjones96@gmail.com>
2025-01-05 17:12:25 -0800

    feat: add /about route

--- src/main/resources/web/src/modules/x/app/app.js
+++ src/main/resources/web/src/modules/x/app/app.js
@@ -1,5 +1,20 @@
 import { LightningElement } from 'lwc';
 
+const routeMap = {
+    "/": "Home",
+    "/about": "About"
+};
+
 export default class App extends LightningElement {
     static renderMode = 'light';
+
+    _path;
+
+    get path() {
+        return routeMap[this._path];
+    }
+
+    connectedCallback() {
+        this._path = window.location.pathname;
+    }
 }
