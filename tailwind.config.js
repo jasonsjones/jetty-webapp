71183b2ea19afe43259fe0833d345f11ab27dd23
Jason Jones <jsjones96@gmail.com>
2025-01-12 15:52:49 -0800

    feat: add links to header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.slate
+            }
+        }
+    },
+    plugins: [],
 }
